fa09de13d829acb76f028aa4dd81c79e085199e7
Barsharanijena <barsharanijena555@gmail.com>
2026-06-22 19:44:52 +0530

    notebook done

---
+++ setup.py
@@ -0,0 +1,11 @@
+fromp setuptools import setup, find_packages
+
+setup(
+    name='medical-chatbot',
+    version='0.1.0',
+    author='Barsharani Jena',
+    author_email='barsharanijena555@gmai.com',
+    description='A medical chatbot using RAG and LangChain',
+    packages=find_packages(),
+    install_requires=[]
+)
